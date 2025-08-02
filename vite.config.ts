53af6517506927170be2b063862d739b0c5056bf
Nacer Berkane <nacer.ayed@outlook.fr>
2025-08-02 18:28:57 +0200

    fixed crypto deployment error

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,20 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { sveltekit } from '@sveltejs/kit/vite'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-	plugins: [sveltekit()]
-});
+	plugins: [sveltekit()],
+	ssr: {
+		noExternal: ['@ai-sdk/google', '@ai-sdk/svelte', 'ai'],
+	},
+	optimizeDeps: {
+		exclude: ['crypto'],
+	},
+	define: {
+		global: 'globalThis',
+	},
+	resolve: {
+		alias: {
+			crypto: 'crypto-browserify',
+		},
+	},
+})
